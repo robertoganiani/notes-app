4ddf6e5abdc7b4ab922b88181e845416d9886c63
Robert Oganiani <oganiani90@gmail.com>
2017-11-28 11:49:51 +0400

    Fix hardware back button

--- App/Navigation/ReduxNavigation.js
+++ App/Navigation/ReduxNavigation.js
@@ -1,17 +1,64 @@
-import React from 'react'
-import * as ReactNavigation from 'react-navigation'
-import { connect } from 'react-redux'
-import AppNavigation from './AppNavigation'
+import React from "react"
+import * as ReactNavigation from "react-navigation"
+import { connect } from "react-redux"
+import AppNavigation from "./AppNavigation"
+import { BackHandler } from "react-native"
+import PropTypes from "prop-types"
 
-// here is our redux-aware our smart component
-function ReduxNavigation (props) {
-  const { dispatch, nav } = props
-  const navigation = ReactNavigation.addNavigationHelpers({
-    dispatch,
-    state: nav
-  })
+class ReduxNavigation extends React.Component {
 
-  return <AppNavigation navigation={navigation} />
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    nav: PropTypes.object.isRequired,
+  }
+
+  _isDrawerOpen = nav => nav.routes[0].index === 1
+
+  _shouldCloseApp = nav => {
+    if (nav.index > 0) return false;
+
+    if (nav.routes) {
+      return nav.routes.every(this._shouldCloseApp);
+    }
+
+    return true;
+  }
+
+  _goBack = () => this.props.dispatch(ReactNavigation.NavigationActions.back())
+
+  _closeDrawer = () => this.props.dispatch(ReactNavigation.NavigationActions.navigate({
+    routeName: "DrawerClose"
+  }))
+
+  _handleBackPress = () => {
+    if (this._isDrawerOpen(this.props.nav)) {
+      this._closeDrawer()
+      return true
+    }
+    if (this._shouldCloseApp(this.props.nav)) {
+      return false
+    }
+    this._goBack()
+    return true
+  }
+
+  componentWillMount() {
+    BackHandler.addEventListener("hardwareBackPress", this._handleBackPress)
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener("hardwareBackPress", this._handleBackPress)
+  }
+
+  render() {
+    const { dispatch, nav } = this.props
+    const navigation = ReactNavigation.addNavigationHelpers({
+      dispatch,
+      state: nav
+    })
+
+    return <AppNavigation navigation={navigation} />
+  }
 }
 
 const mapStateToProps = state => ({ nav: state.nav })
